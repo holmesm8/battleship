17093ca75ee4a9592c0fdf45198585e6c1790105
Mariana Cid <cidmariana5@gmail.com>
2019-12-03 16:58:50 -0700

    Update ship class and test

--- test/ship_test.rb
+++ test/ship_test.rb
@@ -1,7 +1,81 @@
-require '.lib/ship'
+# pry(main)> require './lib/ship'
+# #=> true
+#
+# pry(main)> cruiser = Ship.new("Cruiser", 3)
+# #=> #<Ship:0x00007feb05112d10...>
+#
+# pry(main)> cruiser.name
+# #=> "Cruiser"
+#
+# pry(main)> cruiser.length
+# #=> 3
+#
+# pry(main)> cruiser.health
+# #=> 3
+#
+# pry(main)> cruiser.sunk?
+# #=> false
+#
+# pry(main)> cruiser.hit
+#
+# pry(main)> cruiser.health
+# #=> 2
+#
+# pry(main)> cruiser.hit
+#
+# pry(main)> cruiser.health
+# #=> 1
+#
+# pry(main)> cruiser.sunk?
+# #=> false
+#
+# pry(main)> cruiser.hit
+#
+# pry(main)> cruiser.sunk?
+# #=> true
+#
+require './lib/ship'
 require 'minitest/autorun'
 require 'minitest/pride'
 
 class ShipTest < Minitest::Test
-  
+
+  def test_ship_exists
+    cruiser = Ship.new("Cruiser", 3)
+    assert_instance_of Ship,  cruiser
+  end
+
+  def test_ship_has_attributes
+    cruiser = Ship.new("Cruiser", 3)
+    assert_equal "Cruiser", cruiser.name
+    assert_equal 3, cruiser.length
+  end
+
+  def test_it_has_health
+    cruiser = Ship.new("Cruiser", 3)
+    assert_equal 3, cruiser.health
+  end
+
+  def test_it_has_not_sunk
+    cruiser = Ship.new("Cruiser", 3)
+    assert_equal false, cruiser.sunk?
+  end
+
+  def test_it_can_be_hit
+    cruiser = Ship.new("Cruiser", 3)
+    cruiser.hit
+    assert_equal 2, cruiser.health
+  end
+
+  def test_it_can_take_several_hits_and_sinks
+    cruiser = Ship.new("Cruiser", 3)
+    cruiser.hit
+    assert_equal 2, cruiser.health
+    cruiser.hit
+    assert_equal 1, cruiser.health
+    cruiser.hit
+    assert_equal 0, cruiser.health
+    assert cruiser.sunk?
+    require "pry"; binding.pry
+  end
 end
