a5f5f52a75963cc35ffdd099cf98ed30e22c5999
Matt Holmes <holmesm8@gmail.com>
2019-12-03 15:55:46 -0700

    Create cell and cell test with code

---
+++ test/cell_test.rb
@@ -0,0 +1,63 @@
+# pry(main)> require './lib/ship'
+# # => true
+#
+# pry(main)> require './lib/cell'
+# # => true
+#
+# pry(main)> cell = Cell.new("B4")
+# # => #<Cell:0x00007f84f0ad4720...>
+#
+# pry(main)> cell.coordinate
+# # => "B4"
+#
+# pry(main)> cell.ship
+# # => nil
+#
+# pry(main)> cell.empty?
+# # => true
+#
+# pry(main)> cruiser = Ship.new("Cruiser", 3)
+# # => #<Ship:0x00007f84f0891238...>
+#
+# pry(main)> cell.place_ship(cruiser)
+#
+# pry(main)> cell.ship
+# # => #<Ship:0x00007f84f0891238...>
+#
+# pry(main)> cell.empty?
+# # => false
+#
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/cell'
+require './lib/ship'
+
+class CellTest < Minitest::Test
+
+  def test_cell_exists
+    cell = Cell.new("B4")
+
+    assert_instance_of Cell, cell
+  end
+
+  def test_it_has_attributes
+    cell = Cell.new("B4")
+
+    assert_equal "B4", cell.coordinate
+    assert_equal nil, cell.ship
+
+  end
+
+  def test_cell_is_empty
+    cell = Cell.new("B4")
+
+    assert_equal true, cell.empty?
+  end
+
+  def test_place_ship
+
+
+    assert_equal dsfsd, cell.place_ship
+  end
+
+end
